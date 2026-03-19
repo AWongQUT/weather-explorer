9a93ec0e49061dbbe5f4fe36504fd346bd036fcd
Angus Wong <wongas@qut.edu.au>
2026-03-19 03:15:37 +0000

    Fix responsive layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,9 @@ import React from 'react';
 import { Link } from 'react-router';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
 
 function Home() {
 	return (
